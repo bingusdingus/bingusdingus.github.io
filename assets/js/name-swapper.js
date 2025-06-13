7ccd3dbb0dfe3179bc87f76817715ab86aff1d26
bingusdingus <bradleenorris850@gmail.com>
2025-06-12 23:40:38 -0500

    nameswapper

--- assets/js/name-swapper.js
+++ assets/js/name-swapper.js
@@ -1,9 +1,27 @@
 const names = ['blucheese', 'blu_cheese', 'Aperature Science Prototype XR7', 'stinkysteve95', 'waffleboy53', 'farmerbill1982', 'BatmanFan33039'];
 const nameDisplay = document.getElementById('name-display');
+const nameDisplayBar = document.getElementById('name-display-bar');
 
 let usedNames = ['blucheese'];
 
-const interval = setInterval(pickNewName, 5000);
+const duration = 5000;
+let zero = document.timeline.currentTime;
+
+requestAnimationFrame(progressTowardsNextName);
+
+function progressTowardsNextName(timestamp) {
+    const value = (timestamp - zero) / duration;
+    const percent = value * 100;
+
+    nameDisplayBar.style.width = (percent > 100) ? '100%' : `${percent}%`;
+
+    if (value > 1) {
+        zero = timestamp;
+        pickNewName();
+    }
+
+    requestAnimationFrame((t) => progressTowardsNextName(t));
+}
 
 function pickNewName() {
     let filteredNames = names.filter((name) => !usedNames.includes(name));
