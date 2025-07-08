8ad69b50d02bbc7bb6137d730f9d9628b132bb88
bingusdingus <bradleenorris850@gmail.com>
2025-07-08 11:21:30 -0500

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,16 @@ document.querySelectorAll('a.link').forEach(e => {
     if (e.getAttribute('href') == window.location.pathname)
         e.classList.add('active');
 });
+
+document.getElementById('switch-theme').addEventListener('click', e => {
+    const html = document.querySelector('html');
+    const currentTheme = html.getAttribute('data-bs-theme');
+
+    html.setAttribute('data-bs-theme', (currentTheme == 'light') ? 'dark' : 'light');
+
+    const currentThemeIcon = document.getElementById('switch-theme-icon');
+    const currentThemeIconClass = currentThemeIcon.className;
+
+    console.log(currentThemeIconClass);
+    currentThemeIcon.className = (currentThemeIconClass == 'bi bi-moon-fill') ? 'bi bi-sun-fill' : 'bi bi-moon-fill'
+})
